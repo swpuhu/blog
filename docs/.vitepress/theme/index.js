af9fa0a528ee28b80dd17f538aa0d639ff015d68
swpuhu <swpujita@126.com>
2023-02-25 23:22:15 +0800

    fix bug

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,11 +1,17 @@
 import DefaultTheme from 'vitepress/theme';
-import SourceCodeExample from '../../components/SourceCodeExample.vue';
+import SourceCodeExample from '../../scripts/SourceCodeExample.vue';
+import AffineExample from '../../components/AffineExample.vue';
+import WebGLExample1 from '../../scripts/webgl/WebGLExample1.vue';
+import WebGLExample3 from '../../scripts/webgl/WebGLExample3.vue';
 import './Layout.css';
 export default {
     ...DefaultTheme,
     enhanceApp(ctx) {
         DefaultTheme.enhanceApp(ctx);
         ctx.app.component('SourceCodeExample', SourceCodeExample);
+        ctx.app.component('AffineExample', AffineExample);
+        ctx.app.component('WebGLExample1', WebGLExample1);
+        ctx.app.component('WebGLExample3', WebGLExample3);
     },
     setup() {},
 };
