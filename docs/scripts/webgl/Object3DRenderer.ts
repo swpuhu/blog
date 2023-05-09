ee0a87af9f1e05c9de396e7881c0405088094f0c
swpuhu <swpujita@126.com>
2023-05-09 18:26:04 +0800

    add: loadModel

---
+++ docs/scripts/webgl/Object3DRenderer.ts
@@ -0,0 +1,7 @@
+export class Object3DRenderer {
+    constructor(
+        private positions?: Iterable<number>,
+        private normals?: Iterable<number>,
+        private indices?: Iterable<number>
+    ) {}
+}
